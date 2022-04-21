3a64a51f56b0edab5fcdfd257981b80e980dc534
EmanuelAbleitner <ableit190030@sr.htlweiz.at>
2022-04-21 07:45:02 +0200

    added more startup parameters

--- wrapper.py
+++ wrapper.py
@@ -1,10 +1,8 @@
 import sys
 from time import sleep
-from pexpect import popen_spawn
 from threading import Thread
 import os
 import os.path
-import pexpect
 
 from server import Server
 
@@ -20,25 +18,8 @@ class Wrapper():
         Thread(target=self._output_reader).start()
 
     def startup(self):
-        if "-f" in sys.argv:
-            index = sys.argv.index("-f")
-            self.server.start(sys.argv[index + 1])
-        else:
-            args = {
-                "java": "java",
-                "ram": "-Xmx4G",
-                "serverjar": "server.jar"
-            }
-            if "-s" in sys.argv:
-                index = sys.argv.index("-s")
-                args["serverjar"] = sys.argv[index + 1]
-            if "-j" in sys.argv:
-                index = sys.argv.index("-j")
-                args["java"] = sys.argv[index + 1]
-            if "-r" in sys.argv:
-                index = sys.argv.index("-r")
-                args["ram"] = sys.argv[index + 1]
-            self.server.start(" ".join((args["java"], args["ram"], "-jar", args["serverjar"], "nogui")))
+        self._edit_properties()
+        self.server.start(self._get_start_command())
 
     def send_command(self, command, wait_time=0, printout=True):
         if len(command) == 0:
@@ -58,6 +39,48 @@ class Wrapper():
     def server_running(self):
         return self.server.is_running()
 
+    def _get_start_command(self):
+        args = {
+            "java": "java",
+            "ram": "4G",
+            "serverjar": "server.jar"
+        }
+
+        if "-jar" in sys.argv:
+            index = sys.argv.index("-jar")
+            args["serverjar"] = sys.argv[index + 1]
+        if "-java" in sys.argv:
+            index = sys.argv.index("-java")
+            args["java"] = sys.argv[index + 1]
+        if "-ram" in sys.argv:
+            index = sys.argv.index("-ram")
+            args["ram"] = sys.argv[index + 1]
+
+        return " ".join((args["java"], "-Xmx" + args["ram"], "-jar", args["serverjar"], "nogui"))
+
+    def _edit_properties(self):
+        if not os.path.exists("./server.properties"):
+            tempserver = Server()
+            tempserver.start(self._get_start_command())
+            tempserver.stop()
+
+        with open("./server.properties", "r") as properties:
+            lines = properties.readlines()
+
+        if "-port" in sys.argv:
+            for line in lines:
+                if "server-port=" in line:
+                    index = lines.index(line)
+                    lines[index] = "server-port=" + sys.argv[sys.argv.index("-port") + 1] + "\n"
+        if "-maxp" in sys.argv:
+            for line in lines:
+                if "max-players=" in line:
+                    index = lines.index(line)
+                    lines[index] = "max-players=" + sys.argv[sys.argv.index("-maxp") + 1] + "\n"
+
+        with open("./server.properties", "w") as properties:
+            properties.writelines(lines)
+
     def _output_reader(self):
         for item in self.server.read_output():
             self._format_and_print(item)
@@ -73,9 +96,6 @@ class Wrapper():
                 except UnicodeDecodeError:
                     pass
                 except AttributeError:
-                    # print(output)
-                    # print(char)
-                    # print(type(char))
                     pass
             output_str = output_str.replace("\n", "")
         if output_str != "":
@@ -84,3 +104,14 @@ class Wrapper():
                     print(item)
             with open("mcserverlogs.txt", "a") as logfile:
                 logfile.write(output_str.replace("\r", "\n"))
+
+# teststartcommand: 
+# mcserverwrapper -jar paper-1.18.2-277.jar -java java -ram 8G -port 25566 -maxp 5
+
+# Valid arguments: 
+# -java: the path to the java.exe executable (default: java)
+# -jar: the server jar file (default: server.jar)
+# -ram: the amount of ram the server should use (default: 4G)
+# -port: the port the server should use (default: 25565)
+# -maxp: the max amount of online players at the same time (default: 20)
+# -whitelist: a list of banned players
